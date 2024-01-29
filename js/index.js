353acc09bde21fbf85ca01e13994cc042158ddd5
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-01-29 14:28:17 +0000

    reser

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 import * as MaterialComponents from 'https://lukeplays33.github.io/material-Components/Material_Components/imports.js';
 
 let restart = document.getElementById('restart');
+let popup = document.getElementById('reset')
 
 restart.onclick = function () {
-    MaterialComponents.openPopup(document.getElementById('reset'));
+    MaterialComponents.openPopup(popup);
+}
+
+popup.onSubmit = function (data) {
+    alert(data);
 }
