f6174621329101e7f10165f5483c7b93d34971c5
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-02-11 18:45:49 +0000

    test

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import * as MaterialComponents from 'https://lukeplays33.github.io/material-Components/Material_Components/imports.js';
 import { colourRandom } from '../utils/colours.js';
 
+window.onload = function () {
 MaterialComponents.initSettings({
     themeColour:colourRandom()
-})
+});
+}
 
 let restart = document.getElementById('restart');
 let popup = document.getElementById('reset')
