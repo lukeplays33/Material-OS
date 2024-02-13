78859060c485b3e357f745809721b7ea87cb1f3a
= <lukehoogenboom@gmail.com>
2024-02-13 08:53:18 +0100

    pagination setup test

---
+++ js/setUpNavigation.js
@@ -0,0 +1,20 @@
+let pages = [
+    '../set-up/welcome.html',
+    '../set-up/browserSelection.html'
+];
+
+let iframe = document.getElementById('setUpPages').;
+
+let index = 0;
+
+document.getElementById('next'),onclick = function () {
+    index++;
+
+ iframe.src = pages[index];
+}
+
+document.getElementById('previous'),onclick = function () {
+    index--;
+
+ iframe.src = pages[index];
+}
